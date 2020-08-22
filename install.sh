a568af408bf31f2a5543ed31401a1c0b55b38f67
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-08-22 23:09:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ apt install python3
 apt install python3-pip
 wget https://github.com/ghost5egy/encrypted-reverse-shell/archive/master.zip
 unzip master.zip
+pip3 uninstall PyCrypto
 pip3 install -U PyCryptodome
